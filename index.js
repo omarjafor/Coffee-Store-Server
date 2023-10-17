c665ee3254c85e4e42be31c8411951b48b9c5c17
omarjafor <omarjaforchy@gmail.com>
2023-10-17 17:02:23 +0600

    update one get one

--- index.js
+++ index.js
@@ -112,6 +112,12 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         })
+        app.get('/managers/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id)}
+            const result = await managerCollection.findOne(query);
+            res.send(result);
+        })
         app.post('/managers', async(req, res) => {
             const manager = req.body;
             const result = await managerCollection.insertOne(manager);
